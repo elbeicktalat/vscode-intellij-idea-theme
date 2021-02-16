4ac5a8face7438e2963fe2b16473841a4c5ff8f9
elbeicktalat <talatelbeick@gmail.com>
2021-02-16 22:27:14 +0100

    add color for the top elements

--- src/colors.js
+++ src/colors.js
@@ -1,4 +1,38 @@
+let white = "#ffffff";
+let black = "#000000";
+let gray = "#dadada";
+let primary = "#2675bf";
+let background = "#ffffff";
+let inactiveBackground = "#f2f2f2";
+let activeBackground = "#eeeeee";
+let foreground = "#333333";
+let inactiveForeground = "#6e6e6e";
+let border = "#cccccc";
+
+
 module.exports = colors = {
-    "editor.background": "#f5f5f5",
-    "editor.foreground": "#333333",
+    "titleBar.activeBackground": activeBackground,
+    "titleBar.activeForeground": foreground,
+    "titleBar.inactiveBackground": activeBackground,
+    "titleBar.inactiveForeground": foreground,
+    "titleBar.border": border,
+
+    "menu.background": activeBackground,
+    "menu.foreground": foreground,
+    "menu.border": border,
+    "menu.selectionBackground": primary,
+    "menu.selectionBorder": border,
+    "menu.selectionForeground": white,
+    "menu.separatorBackground": black,
+
+    "tab.activeBackground": background,
+    "tab.activeBorder": primary,
+    "tab.inactiveBackground": inactiveBackground,
+    "tab.hoverBackground": gray,
+
+    "activityBar.background": background,
+    "activityBar.foreground": primary,
+    "activityBar.activeBackground": activeBackground,
+    "activityBar.inactiveForeground": inactiveForeground,
+    "activityBar.activeBorder": primary,
 };
