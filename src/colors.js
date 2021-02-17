746ae98fe3eff72e40b216e1e7085b44ca85c004
elbeicktalat <talatelbeick@gmail.com>
2021-02-17 17:27:30 +0100

    add sidebar colors & other

--- src/colors.js
+++ src/colors.js
@@ -1,6 +1,7 @@
 let white = "#ffffff";
 let black = "#000000";
 let gray = "#dadada";
+let transparent = "#ffffff00";
 let primary = "#2675bf";
 let background = "#ffffff";
 let inactiveBackground = "#f2f2f2";
@@ -9,9 +10,9 @@ let foreground = "#333333";
 let inactiveForeground = "#6e6e6e";
 let border = "#cccccc";
 let selectionBackground = "#a6d2ff";
-let gitAdded = "#a1e84e";
-let gitModified = "#ffb355";
-let gitDeleted = "#fe6365";
+let gitAdded = "#86c53d";
+let gitModified = "#ff9028";
+let gitDeleted = "#ee4b49";
 
 module.exports = colors = {
     "titleBar.activeBackground": activeBackground,
@@ -35,7 +36,6 @@ module.exports = colors = {
 
     "activityBar.background": background,
     "activityBar.foreground": primary,
-    "activityBar.activeBackground": activeBackground,
     "activityBar.inactiveForeground": inactiveForeground,
     "activityBar.activeBorder": primary,
 
@@ -54,8 +54,26 @@ module.exports = colors = {
     "editorGutter.addedBackground": gitAdded,
     "editorGutter.modifiedBackground": gitModified,
     "editorGutter.deletedBackground": gitDeleted,
+    "editorOverviewRuler.background": background,
+    "editorOverviewRuler.border": white,
     "gitDecoration.addedResourceForeground": gitAdded,
     "gitDecoration.modifiedResourceForeground": gitModified,
     "gitDecoration.deletedResourceForeground": gitDeleted,
-    "gitDecoration.conflictingResourceForeground": "#6e8191"
+    "gitDecoration.conflictingResourceForeground": "#6e8191",
+
+    "sideBar.background": white,
+    "sideBar.border": gray,
+    "sideBar.foreground": foreground,
+    "sideBarSectionHeader.background": white,
+    "scrollbarSlider.background": gray,
+    
+    "input.background": gray,
+    "input.foreground": foreground,
+    "input.border": border,
+    
+    "focusBorder": transparent,
+
+    "statusBar.background": gray,
+    "statusBar.foreground": foreground,
+    "statusBar.border": border,
 };
